7a6e2f2f4162315e3639125b058cb43938b37460
Julien <julien.etelain@outlook.com>
2021-07-12 23:00:00 +0200

    Disable debug mode

--- addons/gearinfo/script_component.hpp
+++ addons/gearinfo/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT gearinfo
 #include "\z\aceax\addons\main\script_mod.hpp"
 
- #define DEBUG_MODE_FULL
- #define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_GEARINFO
     #define DEBUG_MODE_FULL
--- addons/ingame/script_component.hpp
+++ addons/ingame/script_component.hpp
@@ -1,8 +1,8 @@
 #define COMPONENT ingame
 #include "\z\aceax\addons\main\script_mod.hpp"
 
- #define DEBUG_MODE_FULL
- #define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
 
 #ifdef DEBUG_ENABLED_INGAME
     #define DEBUG_MODE_FULL
