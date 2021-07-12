41eba007ffd7274fe89e668c4472d17acbb724c3
Julien <julien.etelain@outlook.com>
2021-07-12 21:25:10 +0200

    Adds USP Ops-Core Fast MT

--- compats/compat_usp/XtdGearModels.hpp
+++ compats/compat_usp/XtdGearModels.hpp
@@ -262,6 +262,38 @@ class XtdGearModels
 				values[] = {"front","back"};
 			};
 		};
+
+		
+		class USP_OPSCORE_FAST
+		{
+			label = "Ops-Core Fast MT";
+			author = "UnderSiege Productions";
+			options[] = {"camo", "comms","cmopt","goggles","light","counterweight"};
+			class camo
+			{
+				values[] = {"TAN", "OD", "MC", "ABU", "AOR1", "AOR2", "CBR", "DCU", "FLECKTARN", "KTBANSHEE", "KTHIGHLANDER", "KTNEPTUNE", "KTNOMAD", "KTTYPHON", "KTYETI", "M81", "MCA", "MCD", "MCT", "RGR", "TGS", "TROPENTARN", "UCP"};
+			};
+			class comms
+			{
+				values[] = {"none","CT"};
+			};
+			class cmopt
+			{
+				values[] = {"none","CM"};
+			};
+			class goggles
+			{
+				values[] = {"none","GG"};
+			};
+			class light
+			{
+				values[] = {"none","SF","TL"};
+			};
+			class counterweight
+			{
+				values[] = {"none","CW"};
+			};
+		};
 	};
 
 };
